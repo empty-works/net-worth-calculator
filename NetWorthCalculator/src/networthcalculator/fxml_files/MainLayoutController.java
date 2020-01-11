51ad6404f2d022dba3e47f5c1c87cbe1f73cc670
empty-works <pama.mark@gmail.com>
2020-01-11 23:41:31 +0100

    Set up the FXML file for the main layout as well as its controller and CSS file.

---
+++ NetWorthCalculator/src/networthcalculator/fxml_files/MainLayoutController.java
@@ -0,0 +1,21 @@
+/*
+ */
+package networthcalculator.fxml_files;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author MP
+ */
+public class MainLayoutController implements Initializable {
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
