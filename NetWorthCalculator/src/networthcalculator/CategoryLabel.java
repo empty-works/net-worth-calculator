c3a83e1ffb6395d5ca1d2080a1353f132691b2dd
empty-works <pama.mark@gmail.com>
2020-01-24 15:26:10 +0100

    Set background of CategoryLabel using background fills, paint, cornerradii and insets.

--- NetWorthCalculator/src/networthcalculator/CategoryLabel.java
+++ NetWorthCalculator/src/networthcalculator/CategoryLabel.java
@@ -2,11 +2,30 @@
  */
 package networthcalculator;
 
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
 /**
  *
  * @author MP
  */
 public class CategoryLabel extends javafx.scene.control.Label {
     
+    public CategoryLabel() {
+        
+        setBackground();
+    }
     
+    private void setBackground() {
+        
+        BackgroundFill bgf = new BackgroundFill(
+                Paint.valueOf("orange"), 
+                new CornerRadii(0.5), 
+                new Insets(0.5, 0.5, 0.5, 0.5));
+        this.setBackground(new Background(bgf));
+    }
 }
