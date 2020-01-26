2e7dc5de52a94befa889451145efd8213fdd0220
empty-works <pama.mark@gmail.com>
2020-01-26 23:57:19 +0100

    Set the size and background properties for AmountLabel. Also changed the class to an AnchorPane layout.

--- NetWorthCalculator/src/networthcalculator/labels/AmountLabel.java
+++ NetWorthCalculator/src/networthcalculator/labels/AmountLabel.java
@@ -2,11 +2,41 @@
  */
 package networthcalculator.labels;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Paint;
+
 /**
  *
  * @author MP
  */
-public class AmountLabel extends javafx.scene.control.Label {
+public class AmountLabel extends javafx.scene.layout.AnchorPane {
     
+    private Label label = new Label();
     
+    public AmountLabel(String text) {
+        
+        System.out.println("Label text: " + text);
+        label.setText(text);
+        setBackground();
+        setSetSize();
+    }
+    
+    private void setSetSize() {
+        
+        this.setPrefHeight(65);
+        
+    }
+    
+    private void setBackground() {
+        
+        BackgroundFill bgf = new BackgroundFill(
+                Paint.valueOf("red"), 
+                new CornerRadii(0.5), 
+                new Insets(0.5, 0.5, 0.5, 0.5));
+        this.setBackground(new Background(bgf));
+    }
 }
