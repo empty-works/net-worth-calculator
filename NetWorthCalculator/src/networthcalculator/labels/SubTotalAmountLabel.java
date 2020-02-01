1db174a5b436e9c4cd31028620824063e44b2422
empty-works <pama.mark@gmail.com>
2020-02-01 21:45:47 +0100

    Added a Label and text variable to SubTotalAmountLabel. Also set max width and label properties for the subTotal Label.

--- NetWorthCalculator/src/networthcalculator/labels/SubTotalAmountLabel.java
+++ NetWorthCalculator/src/networthcalculator/labels/SubTotalAmountLabel.java
@@ -2,11 +2,32 @@
  */
 package networthcalculator.labels;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import networthcalculator.SetSizes;
+
 /**
  *
  * @author MP
  */
-public class SubTotalAmountLabel {
+public class SubTotalAmountLabel extends javafx.scene.layout.AnchorPane {
     
+    private Label subTotal = new Label();
+    private String text;
+    final private int PREF_HEIGHT = 25;
     
+    public SubTotalAmountLabel(String text) {
+        
+        this.text = text;
+        setLabelProperties();
+        this.setMaxWidth(SetSizes.MAX_WIDTH);
+    }
+    
+    private void setLabelProperties() {
+        
+        this.getChildren().add(subTotal);
+        subTotal.setPrefHeight(PREF_HEIGHT);
+        AnchorPane.setLeftAnchor(subTotal, 0.0);
+        AnchorPane.setRightAnchor(subTotal, 0.0);
+    }
 }
