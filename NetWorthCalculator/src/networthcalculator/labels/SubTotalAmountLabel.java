b7702092fbdbe0000251213a1be5d9137f998bf7
empty-works <pama.mark@gmail.com>
2020-02-02 22:06:10 +0100

    Set padding for each label in SubTotalAmountLabel as well as set font proprties in the CSS.

--- NetWorthCalculator/src/networthcalculator/labels/SubTotalAmountLabel.java
+++ NetWorthCalculator/src/networthcalculator/labels/SubTotalAmountLabel.java
@@ -17,7 +17,7 @@ public class SubTotalAmountLabel extends javafx.scene.layout.AnchorPane {
     
     private Label leftPlaceholder = new Label();
     private Label subTotal = new Label();
-    private Label rightPlaceholder = new Label();
+    private Label subAmountNumLabel = new Label();
     private HBox hbox = new HBox();
     private String text;
     final private int PREF_HEIGHT = 25;
@@ -36,7 +36,7 @@ public class SubTotalAmountLabel extends javafx.scene.layout.AnchorPane {
         
         hbox.setId("SubTotalAmountLabelHBox");
         ObservableList list = hbox.getChildren();
-        list.addAll(leftPlaceholder, subTotal, rightPlaceholder);
+        list.addAll(leftPlaceholder, subTotal, subAmountNumLabel);
         AnchorPane.setLeftAnchor(hbox, 0.0);
         AnchorPane.setRightAnchor(hbox, 0.0);
     }
@@ -47,12 +47,14 @@ public class SubTotalAmountLabel extends javafx.scene.layout.AnchorPane {
         leftPlaceholder.setAlignment(Pos.CENTER_LEFT);
         leftPlaceholder.prefWidthProperty().bind(hbox.widthProperty());
 
+        subTotal.setId("SubTotalLabel");
         subTotal.setText(text + " " + Constants.CURRENCY);
         subTotal.setPrefHeight(PREF_HEIGHT);
         subTotal.prefWidthProperty().bind(hbox.widthProperty());
         
-        rightPlaceholder.setPrefHeight(PREF_HEIGHT);
-        rightPlaceholder.setAlignment(Pos.CENTER_RIGHT);
-        rightPlaceholder.prefWidthProperty().bind(hbox.widthProperty());
+        subAmountNumLabel.setText("10000000.00");
+        subAmountNumLabel.setPrefHeight(PREF_HEIGHT);
+        subAmountNumLabel.setAlignment(Pos.CENTER_RIGHT);
+        subAmountNumLabel.prefWidthProperty().bind(hbox.widthProperty());
     }
 }
