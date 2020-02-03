3f586c4e30a5de069284694a9f7a9ff24851e581
empty-works <pama.mark@gmail.com>
2020-02-03 21:07:45 +0100

    Removed code in MainUi that sets grid properties. Also removed the grid, scroll pane within MainUi and related imports.

--- NetWorthCalculator/src/networthcalculator/MainUi.java
+++ NetWorthCalculator/src/networthcalculator/MainUi.java
@@ -3,17 +3,9 @@
 package networthcalculator;
 
 import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import networthcalculator.labels.CategoryLabel;
 import javafx.scene.layout.ColumnConstraints;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
-import networthcalculator.labels.AmountLabel;
-import networthcalculator.labels.SubCategoryLabel;
-import networthcalculator.labels.SubTotalAmountLabel;
 
 /**
  *
@@ -21,13 +13,9 @@ import networthcalculator.labels.SubTotalAmountLabel;
  */
 public class MainUi extends javafx.scene.layout.GridPane {
     
-    private ScrollPane assetsScrollPane = new ScrollPane();
-    private GridPane assetsGridPane = new GridPane();
-    
     public MainUi() {
         
         setColumnConstraints();
-        setGridProperties();
         addLabels();
     }
     
@@ -39,12 +27,6 @@ public class MainUi extends javafx.scene.layout.GridPane {
         column1.setHalignment(HPos.CENTER);
         this.getColumnConstraints().addAll(column1);
         System.out.println("Added column constraints to grid pane...");
-    }
-    
-    private void setGridProperties() {
-        
-        assetsGridPane.setAlignment(Pos.CENTER);
-        assetsGridPane.setPadding(new Insets(10, 0, 0, 0)); // Adds space to the top.
     }
     
     private void addLabels() {
