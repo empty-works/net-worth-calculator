06665d308d859baaf202abba3986ff2aa140fe81
empty-works <pama.mark@gmail.com>
2020-02-03 20:23:58 +0100

    The assets title has been moved to the top of the grid. A scroll pane has been added to the second row. This scroll pane adds another grid pane which adds all asset labels.

--- NetWorthCalculator/src/networthcalculator/MainUi.java
+++ NetWorthCalculator/src/networthcalculator/MainUi.java
@@ -5,6 +5,8 @@ package networthcalculator;
 import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import networthcalculator.labels.CategoryLabel;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
@@ -19,14 +21,15 @@ import networthcalculator.labels.SubTotalAmountLabel;
  */
 public class MainUi extends javafx.scene.layout.GridPane {
     
-    private GridPane grid = new GridPane();
+    private ScrollPane assetsScrollPane = new ScrollPane();
+    private GridPane assetsGridPane = new GridPane();
     
     public MainUi() {
         
         setColumnConstraints();
         setGridProperties();
-        addLabels();
         setScrollPaneProperties();
+        addLabels();
     }
     
     private void setColumnConstraints() {
@@ -35,35 +38,36 @@ public class MainUi extends javafx.scene.layout.GridPane {
         column1.setPercentWidth(100);
         column1.setHgrow(Priority.ALWAYS);
         column1.setHalignment(HPos.CENTER);
-        grid.getColumnConstraints().addAll(column1);
+        assetsGridPane.getColumnConstraints().addAll(column1);
         System.out.println("Added column constraints to grid pane...");
     }
     
     private void setGridProperties() {
         
-        grid.setAlignment(Pos.CENTER);
-        grid.setPadding(new Insets(10, 0, 0, 0)); // Adds space to the top.
+        assetsGridPane.setAlignment(Pos.CENTER);
+        assetsGridPane.setPadding(new Insets(10, 0, 0, 0)); // Adds space to the top.
     }
     
     private void addLabels() {
         
         // ASSETS
-        grid.add(new CategoryLabel("ASSETS", "CURRENT VALUE"), 0, 0);
-        grid.add(new SubCategoryLabel("Cash and Cash Equivalents"), 0, 1);
-        grid.add(new AmountLabel("Checking accounts"), 0, 2);
-        grid.add(new AmountLabel("Savings accounts"), 0, 3);
-        grid.add(new AmountLabel("Money market accounts"), 0, 4);
-        grid.add(new AmountLabel("Savings bonds"), 0, 5);
-        grid.add(new AmountLabel("CD's"), 0, 6);
-        grid.add(new AmountLabel("Cash value of life insurance"), 0, 7);
-        grid.add(new SubTotalAmountLabel("Total Cash"), 0, 8);
+        this.add(new CategoryLabel("ASSETS", "CURRENT VALUE"), 0, 0);
+        this.add(assetsScrollPane, 0, 1);
+        assetsScrollPane.setContent(assetsGridPane);
+        assetsGridPane.add(new SubCategoryLabel("Cash and Cash Equivalents"), 0, 1);
+        assetsGridPane.add(new AmountLabel("Checking accounts"), 0, 2);
+        assetsGridPane.add(new AmountLabel("Savings accounts"), 0, 3);
+        assetsGridPane.add(new AmountLabel("Money market accounts"), 0, 4);
+        assetsGridPane.add(new AmountLabel("Savings bonds"), 0, 5);
+        assetsGridPane.add(new AmountLabel("CD's"), 0, 6);
+        assetsGridPane.add(new AmountLabel("Cash value of life insurance"), 0, 7);
+        assetsGridPane.add(new SubTotalAmountLabel("Total Cash"), 0, 8);
     }
     
     private void setScrollPaneProperties() {
         
-        this.fitToWidthProperty().set(true);
-        this.hbarPolicyProperty().setValue(ScrollBarPolicy.NEVER);
-        this.setPannable(true);
-        this.setContent(grid);
+        assetsScrollPane.fitToWidthProperty().set(true);
+        assetsScrollPane.hbarPolicyProperty().setValue(ScrollBarPolicy.NEVER);
+        assetsScrollPane.setPannable(true);
     }
 }
