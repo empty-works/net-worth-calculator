39b912888f3249b33fab1696102f94df02cbc48f
empty-works <pama.mark@gmail.com>
2020-02-09 13:48:22 +0100

    Removed obsolete code related to changing the height of the bottom border pane. Set appropriate text for the bottom border pane.

--- NetWorthCalculator/src/networthcalculator/containers/BottomNetWorthContainer.java
+++ NetWorthCalculator/src/networthcalculator/containers/BottomNetWorthContainer.java
@@ -14,31 +14,26 @@ public class BottomNetWorthContainer extends javafx.scene.layout.GridPane {
     private Label totalAssets = new Label();
     private Label totalLiabilities = new Label();
     private Label netWorth = new Label();
-    private double PREF_HEIGHT = 100;
     
     public BottomNetWorthContainer() {
         
-        this.prefHeight(PREF_HEIGHT);
-        this.minHeight(PREF_HEIGHT);
-        this.setPadding(new Insets(15));
+        this.setPadding(new Insets(15)); // Allows for height change. Setting height directly does not work.
         
         setLabels();
     }
     
     private void setLabels() {
         
-        //TODO Fix height of labels
-        
         totalAssets.prefWidthProperty().bind(this.widthProperty().divide(3));
-        totalAssets.setText("YOOOOOOOOOOOOOOOOOOOOO");
+        totalAssets.setText("Total Assets:");
         this.add(totalAssets, 0, 0);
         
         totalLiabilities.prefWidthProperty().bind(this.widthProperty().divide(3));
-        totalLiabilities.setText("HEEEEEEEEYYYYYYYYYYYYYYYYYYYYY");
+        totalLiabilities.setText("Total Liabilities:");
         this.add(totalLiabilities, 1, 0);
         
         netWorth.prefWidthProperty().bind(this.widthProperty().divide(3));
-        netWorth.setText("WHAT UP");
+        netWorth.setText("Net Worth:");
         this.add(netWorth, 2, 0);
     }
 }
