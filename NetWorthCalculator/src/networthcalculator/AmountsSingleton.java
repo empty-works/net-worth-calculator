3227d168c4bd4efea5253e00ccbe87ff78a2564b
empty-works <pama.mark@gmail.com>
2020-02-11 16:25:36 +0100

    AmountsSingleton now stores several amounts (total cash, total assets etc.) along with getters and setters.

--- NetWorthCalculator/src/networthcalculator/AmountsSingleton.java
+++ NetWorthCalculator/src/networthcalculator/AmountsSingleton.java
@@ -8,6 +8,13 @@ package networthcalculator;
  */
 public class AmountsSingleton {
   
+    private double totalCash;
+    private double totalInvestedAssets;
+    private double totalUseAssets;
+    private double totalAssets;
+    private double totalLiabilities;
+    private double netWorth;
+    
     private volatile static AmountsSingleton obj;
     
     private AmountsSingleton() {}
@@ -26,5 +33,54 @@ public class AmountsSingleton {
             }
         }
         return obj;
+    }
+
+    
+    public double getTotalCash() {
+        return totalCash;
+    }
+
+    public void setTotalCash(double totalCash) {
+        this.totalCash = totalCash;
+    }
+
+    public double getTotalInvestedAssets() {
+        return totalInvestedAssets;
+    }
+
+    public void setTotalInvestedAssets(double totalInvestedAssets) {
+        this.totalInvestedAssets = totalInvestedAssets;
+    }
+
+    public double getTotalUseAssets() {
+        return totalUseAssets;
+    }
+
+    public void setTotalUseAssets(double totalUseAssets) {
+        this.totalUseAssets = totalUseAssets;
+    }
+
+    public double getTotalAssets() {
+        return totalAssets;
+    }
+
+    public void setTotalAssets(double totalAssets) {
+        this.totalAssets = totalAssets;
+    }
+
+    public double getTotalLiabilities() {
+        return totalLiabilities;
+    }
+
+    public void setTotalLiabilities(double totalLiabilities) {
+        this.totalLiabilities = totalLiabilities;
+    }
+
+    public double getNetWorth() {
+        return netWorth;
+    }
+
+    public void setNetWorth(double netWorth) {
+        this.netWorth = netWorth;
     }
 }
