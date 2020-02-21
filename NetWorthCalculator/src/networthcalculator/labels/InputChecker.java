cba3664066d17a4b666ecc2eb41aa1d3a653bd49
empty-works <pama.mark@gmail.com>
2020-02-21 16:01:10 +0100

    InputChecker checks the value if it is digits only.

--- NetWorthCalculator/src/networthcalculator/labels/InputChecker.java
+++ NetWorthCalculator/src/networthcalculator/labels/InputChecker.java
@@ -11,13 +11,20 @@ import networthcalculator.MyUtility;
  */
 public class InputChecker {
     
-    public static void check(HBox hbox, String bgColor) {
+    public static void check(HBox hbox, String bgColor, String value) {
         
-        
-        
+        // Allow empty string.
+        if(!value.isEmpty()) {
+            
+            // Check if the value only contains digits.
+            if(!value.matches("[0-9]+")) {
+
+                setInvalidBackground(hbox, bgColor);
+            }
+        }
     }
     
-    private void setInvalidBackground(HBox hbox, String bgColor) {
+    private static void setInvalidBackground(HBox hbox, String bgColor) {
         
         hbox.setStyle("-fx-background-color:" + bgColor + "; " + 
                 "-fx-background-radius:" + MyUtility.DEFAULT_CORNER_RADIUS + "; " + 
