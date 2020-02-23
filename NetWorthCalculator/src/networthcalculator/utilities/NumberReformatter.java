2ec37247b27c01af87532667d74f0692aa401ef0
empty-works <pama.mark@gmail.com>
2020-02-23 18:21:55 +0100

    NumberReformatter now formats the user's input with proper commas and decimal places. However, this has caused other problems.

--- NetWorthCalculator/src/networthcalculator/utilities/NumberReformatter.java
+++ NetWorthCalculator/src/networthcalculator/utilities/NumberReformatter.java
@@ -2,6 +2,8 @@
  */
 package networthcalculator.utilities;
 
+import java.text.DecimalFormat;
+
 /**
  *
  * @author MP
@@ -12,13 +14,21 @@ public class NumberReformatter {
     public static String reformat(String inputAmount) {
         
         double dblAmount = Double.parseDouble(inputAmount);
-        String formattedAmount = String.format("%.2f", dblAmount);
+        //String formattedAmount = String.format("%.2f", dblAmount);
+        String formattedAmount = formatDecimal(dblAmount);
         return formattedAmount;
     }
     
     public static String reformat(double amount) {
         
-        String formattedAmount = String.format("%.2f", amount);
+        //String formattedAmount = String.format("%.2f", amount);
+        String formattedAmount = formatDecimal(amount);
         return formattedAmount;
+    }
+    
+    private static String formatDecimal(double amount) {
+        
+        DecimalFormat formatter = new DecimalFormat("#,###.00");
+        return formatter.format(amount);
     }
 }
