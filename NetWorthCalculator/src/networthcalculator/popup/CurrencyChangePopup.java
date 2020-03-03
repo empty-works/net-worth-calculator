3ede922ddffe13654495ad86ebe8a1db448b42dc
empty-works <pama.mark@gmail.com>
2020-03-03 23:51:57 +0100

    Created a VBox object and some Label objects to populate the VBox.

--- NetWorthCalculator/src/networthcalculator/popup/CurrencyChangePopup.java
+++ NetWorthCalculator/src/networthcalculator/popup/CurrencyChangePopup.java
@@ -2,11 +2,26 @@
  */
 package networthcalculator.popup;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
 /**
  *
  * @author MP
  */
-public class CurrencyChangePopup {
+public class CurrencyChangePopup extends javafx.stage.Popup {
     
+    private VBox vbox = new VBox();
     
+    public CurrencyChangePopup() {
+        
+        setVBoxItems();
+    }
+    
+    private void setVBoxItems() {
+        
+        Label label1 = new Label("Label1");
+        Label label2 = new Label("Label2");
+        Label label3 = new Label("Label3");
+    }
 }
