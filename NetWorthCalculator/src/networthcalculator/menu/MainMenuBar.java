90749f1a95e4562849673f7d9ff483f5fe470152
empty-works <pama.mark@gmail.com>
2020-03-04 17:37:31 +0100

    Added file menu and menu items to the menu bar.

--- NetWorthCalculator/src/networthcalculator/menu/MainMenuBar.java
+++ NetWorthCalculator/src/networthcalculator/menu/MainMenuBar.java
@@ -2,11 +2,22 @@
  */
 package networthcalculator.menu;
 
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+
 /**
  *
  * @author MP
  */
 public class MainMenuBar extends javafx.scene.control.MenuBar {
     
-    
+    public MainMenuBar() {
+        
+        // File menu
+        Menu fileMenu = new Menu("File");
+        MenuItem fileItem1 = new MenuItem("New file...");
+        MenuItem fileItem2 = new MenuItem("Save"); 
+        MenuItem fileItem3 = new MenuItem("Open file...");
+        fileMenu.getItems().addAll(fileItem1, fileItem2, fileItem3);
+    }
 }
