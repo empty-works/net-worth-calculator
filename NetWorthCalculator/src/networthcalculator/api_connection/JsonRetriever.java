dcde3254780d580f8a572001c68c9c832c427965
empty-works <pama.mark@gmail.com>
2020-03-07 21:05:22 +0100

    Added correct .jar file for json manipulation.

--- NetWorthCalculator/src/networthcalculator/api_connection/JsonRetriever.java
+++ NetWorthCalculator/src/networthcalculator/api_connection/JsonRetriever.java
@@ -2,24 +2,37 @@
  */
 package networthcalculator.api_connection;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 /**
  *
  * @author MP
  */
 public class JsonRetriever {
     
-    public JsonRetriever() {
+    private String jsonString;
+    
+    public JsonRetriever() throws IOException, FileNotFoundException, ParseException {
         
         setConnection();
     }
     
-    private void setConnection() {
+    private void setConnection() throws FileNotFoundException, IOException, ParseException {
         
         CurrencyAPIController controller = new CurrencyAPIController();
         controller.initialize(null, null);
         
         // Test connection
-        String test = controller.connection();
-        System.out.println("Test connection: " + test);
+        jsonString = controller.connection();
+        System.out.println("Test connection: " + jsonString);
+        
+        
+        // Parse string
+        Object obj = new JSONParser().parse(jsonString);
+        
     }
 }
