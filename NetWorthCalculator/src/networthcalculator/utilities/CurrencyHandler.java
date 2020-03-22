6f22bd1d27972d6b1bf2118bb0870a5019db35ac
empty-works <pama.mark@gmail.com>
2020-03-22 21:32:43 +0100

    Created JUnit test for CurrencyHandler.

--- NetWorthCalculator/src/networthcalculator/utilities/CurrencyHandler.java
+++ NetWorthCalculator/src/networthcalculator/utilities/CurrencyHandler.java
@@ -48,8 +48,7 @@ public class CurrencyHandler {
         convertAmounts();
     }
     
-    // TODO write junit test
-    private void convertAmounts() {
+    public void convertAmounts() {
         
         if(!amountFieldList.isEmpty()) {
             
---
+++ NetWorthCalculator/test/networthcalculator/utilities/CurrencyHandlerTest.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package networthcalculator.utilities;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author MP
+ */
+public class CurrencyHandlerTest {
+    
+    public CurrencyHandlerTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of setCurrencyRate method, of class CurrencyHandler.
+     */
+    @Test
+    public void testSetCurrencyRate() {
+        System.out.println("setCurrencyRate");
+        String currencyRate = "";
+        CurrencyHandler instance = new CurrencyHandler();
+        instance.setCurrencyRate(currencyRate);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+    @Test
+    public void testConvertAmounts() {
+        
+    }
+}
