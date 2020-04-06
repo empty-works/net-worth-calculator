f644f6ee1d7821b1a42b216956a4791fa479741c
empty-works <pama.mark@gmail.com>
2020-04-06 22:08:30 +0200

    Created a class to allow the user to edit the names on the amount label.

---
+++ NetWorthCalculator/src/networthcalculator/labels/EditLabel.java
@@ -0,0 +1,12 @@
+/*
+ */
+package networthcalculator.labels;
+
+/**
+ *
+ * @author MP
+ */
+public class EditLabel extends javafx.scene.control.Button {
+    
+    //TODO this class used to give user ability to edit amount label names.
+}
