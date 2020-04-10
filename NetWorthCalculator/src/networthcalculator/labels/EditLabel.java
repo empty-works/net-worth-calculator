99a8662df7c33c5740e666d054b7434a2151a6c4
empty-works <pama.mark@gmail.com>
2020-04-10 20:27:45 +0200

    Added a set on action event to EditLabel to allow the user to change the name of the amount label.

--- NetWorthCalculator/src/networthcalculator/labels/EditLabel.java
+++ NetWorthCalculator/src/networthcalculator/labels/EditLabel.java
@@ -2,11 +2,34 @@
  */
 package networthcalculator.labels;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 /**
  *
  * @author MP
  */
 public class EditLabel extends javafx.scene.control.Button {
     
-    //TODO this class used to give user ability to edit amount label names.
+    private TextField nameField = new TextField();
+    
+    public EditLabel(Label amountLabelName) {
+    
+        setAction();
+    }
+    
+    private void setAction() {
+    
+        this.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                
+                // Make text field appear here to allow user to type a new 
+                // name in.
+            }
+        });
+    }
 }
