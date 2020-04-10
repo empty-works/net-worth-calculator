f0eab4cde315ce7dcbdcb694a19252ef00861cb5
empty-works <pama.mark@gmail.com>
2020-04-10 20:36:54 +0200

    Set button properties and hbox children of EditLabel.

--- NetWorthCalculator/src/networthcalculator/labels/EditLabel.java
+++ NetWorthCalculator/src/networthcalculator/labels/EditLabel.java
@@ -2,6 +2,7 @@
  */
 package networthcalculator.labels;
 
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
@@ -16,10 +17,28 @@ public class EditLabel extends javafx.scene.layout.HBox {
     
     private TextField nameField = new TextField();
     private Button editButton = new Button();
+    final private int BUTTON_SIZE = 100;
     
     public EditLabel(Label amountLabelName) {
     
+        setButtonSize();
+        setTextFieldSize();
         setAction();
+        setChildren();
+    }
+    
+    private void setButtonSize() {
+    
+        editButton.setMinHeight(BUTTON_SIZE);
+        editButton.setPrefHeight(BUTTON_SIZE);
+        editButton.setMinWidth(BUTTON_SIZE);
+        editButton.setPrefWidth(BUTTON_SIZE);
+        // TODO Set a pencil icon here.
+    }
+    
+    private void setTextFieldSize() {
+    
+        // TODO set text field size.
     }
     
     private void setAction() {
@@ -29,9 +48,15 @@ public class EditLabel extends javafx.scene.layout.HBox {
             @Override
             public void handle(ActionEvent event) {
                 
-                // Make text field appear here to allow user to type a new 
+                // TODO Make text field appear here to allow user to type a new 
                 // name in.
             }
         });
+    }
+    
+    private void setChildren() {
+    
+        ObservableList list = this.getChildren();
+        list.addAll(editButton, nameField);
     }
 }
